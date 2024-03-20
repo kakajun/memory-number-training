0cb977977fb2cb108a33d3c348c4776f9f886ae1
马军 <majun@xiwicloud.com>
2024-03-20 10:35:30 +0800

    🐞 fix:1

--- vite.config.ts
+++ vite.config.ts
@@ -23,9 +23,9 @@ export default defineConfig({
       '~/': `${pathSrc}/`
     }
   },
-  base:
-    process.env.NODE_ENV === 'production' ? '/memory-number-training/' : './',
-  // base: './',
+  // base:
+  //   process.env.NODE_ENV === 'production' ? '/memory-number-training/' : './',
+  base: './',
   css: {
     preprocessorOptions: {
       scss: {
