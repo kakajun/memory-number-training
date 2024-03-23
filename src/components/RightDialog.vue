a7722c0d1cf476b4965ae62a4ae61f250f78b041
马军 <majun@xiwicloud.com>
2024-03-23 23:32:15 +0800

    🐞 fix:

--- src/components/RightDialog.vue
+++ src/components/RightDialog.vue
@@ -21,7 +21,7 @@
   </el-dialog>
 </template>
 <script setup lang="ts">
-import { computed, defineEmits } from 'vue'
+import { computed } from 'vue'
 const emits = defineEmits(['close', 'update:visible'])
 // 接收props
 const props = defineProps({
