abdaa2f5d079030fb42d02d2a8e57dcfc4f71cf3
马军 <majun@xiwicloud.com>
2024-06-10 17:36:25 +0800

    干掉不要的

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,10 @@ import App from './App.vue'
 import router from './router/router'
 import './styles/index.scss'
 import 'uno.css'
-// import "element-plus/theme-chalk/dark/css-vars.css";
-// If you want to use ElMessage, import it.
+
 import 'element-plus/theme-chalk/src/message.scss'
 
 const app = createApp(App)
 app.use(router) // 使用路由
-// app.use(ElementPlus);
+
 app.mount('#app')
